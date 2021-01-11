f2558a3094434ea44d268f338a23ae82f2f3db59
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-11 22:27:37 +0530

    Added the Title.js

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireSnap <span role="img" aria-label="fire-snaps">🔥</span></h1>
+      <h2>Your Pictures</h2>
+      <p>Safely Store Those Beautiful Memories You Made.</p>
+    </div>
+  )
+}
+
+export default Title;
