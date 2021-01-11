08f0c5660222662e5db88904f87c765df4e2b504
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-12 01:18:30 +0530

    Added the Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p className="lead">Developed By Pravunath Singh With ❤️</p>
+        </footer>
+    )
+}
+
+export default Footer
